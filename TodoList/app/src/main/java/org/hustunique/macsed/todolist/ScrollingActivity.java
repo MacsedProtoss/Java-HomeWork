c1909e3f78cccaaf5dfd934298574df9aa53a4df
Macsed Protoss <43907359+MacsedProtoss@users.noreply.github.com>
2019-11-09 20:14:04 +0800

    add new ClickListener Of Editbtn & add behavior of add_reminder UI

--- TodoList/app/src/main/java/org/hustunique/macsed/todolist/ScrollingActivity.java
+++ TodoList/app/src/main/java/org/hustunique/macsed/todolist/ScrollingActivity.java
@@ -1,5 +1,8 @@
 package org.hustunique.macsed.todolist;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,9 +11,15 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -25,8 +34,55 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Context context = ScrollingActivity.this;
+                LayoutInflater inflater = getLayoutInflater();
+
+                AlertDialog.Builder builder = new  AlertDialog.Builder(context);
+                builder.setTitle("");
+
+                View dialogLayout = inflater.inflate(R.layout.add_new_reminder, null);
+                builder.setView(dialogLayout);
+
+                builder.setPositiveButton("添加", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
+                final AlertDialog dialog = builder.create();
+
+                final Spinner spinner = dialogLayout.findViewById(R.id.add_spin);
+                final EditText repeatTimeText = dialogLayout.findViewById(R.id.add_repeatTime);
+                final EditText strideText = dialogLayout.findViewById(R.id.add_stride);
+                final TextView subText = dialogLayout.findViewById(R.id.add_sub);
+
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        if (position == 0){
+                            repeatTimeText.setVisibility(View.INVISIBLE);
+                            strideText.setVisibility(View.INVISIBLE);
+                            subText.setVisibility(View.INVISIBLE);
+                        }else if (position == 1){
+                            repeatTimeText.setVisibility(View.VISIBLE);
+                            strideText.setVisibility(View.VISIBLE);
+                            subText.setVisibility(View.INVISIBLE);
+                        }else if (position == 2){
+                            repeatTimeText.setVisibility(View.INVISIBLE);
+                            strideText.setVisibility(View.INVISIBLE);
+                            subText.setVisibility(View.VISIBLE);
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+
+                dialog.show();
             }
         });
     }
